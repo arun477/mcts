e7f9059d388c707ff150601fca2dbdcb54ab8700
carlfeynman <coolarun477@gmail.com>
2024-07-28 13:42:20 +0530

    full code added

--- mct_try.py
+++ mct_try.py
@@ -19,6 +19,7 @@ class VanilaMCTS:
             }
         }
         self.total_play = 0
+        self.win_mark = 2
 
     
     def get_ucb_score(self, state):
@@ -71,7 +72,57 @@ class VanilaMCTS:
         child_action_id = child[random_action_choice]
         return child_action_id, len(child_action_id)
     
-    
+
+    def is_terminal_state(self, leaf_state):
+        def __who_wins(sums, win_mark):
+            if np.any(sums == win_mark):
+                return "o"
+            if np.any(sums == -win_mark):
+                return "x"
+            return None
+
+        def __is_terminal_in_conv(leaf_state, win_mark):
+            for axis in range(2):
+                sums = np.sum(leaf_state, axis=axis)
+                result = __who_wins(sums, win_mark)
+                if result is not None:
+                    return result
+
+            for order in [-1, 1]:
+                diags_sum = np.sum(np.diag(leaf_state[::order]))
+                result = __who_wins(diags_sum, win_mark)
+                if result is not None:
+                    return result
+
+            return None
+
+        win_mark = self.win_mark
+        n_rows_board = len(self.state_tree[(0,)]["state"])
+        window_size = win_mark
+        window_positions = range(n_rows_board - win_mark + 1)
+        for row in window_positions:
+            for col in window_positions:
+                window = leaf_state[row : row + window_size, col : col + window_size]
+                winner = __is_terminal_in_conv(window, win_mark)
+                if winner is not None:
+                    return winner
+
+        if not np.any(leaf_state == 0):
+            return "draw"
+
+        return None
+
+    def get_available_next_actions(self, leaf_state):
+        actions = []
+        count = 0
+        state_size = len(leaf_state)
+        for i in range(state_size):
+            for j in range(state_size):
+                if leaf_state[i][j] == 0:
+                    actions.append([(i, j), count])
+                count += 1
+        return actions
+
     def simulate_play(self, child_action_id):
         state = deepcopy(self.state_tree[child_action_id])
         winner = self.is_terminal_state(self.state_tree[child_action_id]['state'])
