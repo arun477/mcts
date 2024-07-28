b8d4114de9fa61a1e33c871f980b86fc28936f8d
carlfeynman <coolarun477@gmail.com>
2024-07-28 14:04:37 +0530

    full play replicated

--- mct_try.py
+++ mct_try.py
@@ -3,10 +3,10 @@ import numpy as np
 from copy import deepcopy
 
 class VanilaMCTS:
-    def __init__(self, initial_state, player, num_iterations=100, max_depth=5, exploit_const=5):
+    def __init__(self, initial_state=None, player=None, num_iterations=100, max_depth=5, explore_const=5, win_mark=3):
         self.num_itearions = num_iterations
         self.max_depth = max_depth
-        self.exploit_const = exploit_const
+        self.explore_const = explore_const
         self.root_id = (0,)
         self.state_tree = { self.root_id: {
                 'child': [],
@@ -19,14 +19,15 @@ class VanilaMCTS:
             }
         }
         self.total_play = 0
-        self.win_mark = 2
+        n_rows = len(initial_state)
+        self.n_rows = n_rows
+        self.win_mark = win_mark
 
     
     def get_ucb_score(self, state):
         exploitation_val = state['w'] / state['n']
         exploration_val = np.sqrt(np.log(self.total_play)/ state['n'])
-        return exploitation_val + self.exploit_const * exploration_val
-
+        return exploitation_val + self.explore_const * exploration_val
     
     def select_potential_leaf_action(self):
         selected_action_id = self.root_id
@@ -35,18 +36,18 @@ class VanilaMCTS:
             parent_id = selected_action_id
             best_score, best_action_id = float('-inf'), None
             for action_id in selected_action_state['child']:
-                action_state = self.state_tree[parent_id + action_id]
+                action_state = self.state_tree[parent_id + (action_id,)]
                 ucb_score = self.get_ucb_score(action_state)
                 if ucb_score > best_score:
                     best_score = ucb_score
                     best_action_id = action_id
-            selected_action_id =parent_id + best_action_id
+            selected_action_id =parent_id + (best_action_id,)
             selected_action_state = self.state_tree[selected_action_id]
         return selected_action_id
 
     
     def expand(self, selected_action_id):
-        if self.is_terminal_state(self.state_tree[selected_action_id]):
+        if self.is_terminal_state(self.state_tree[selected_action_id]['state']):
             return selected_action_id
         
         state = deepcopy(self.state_tree[selected_action_id])
@@ -54,7 +55,7 @@ class VanilaMCTS:
         child = []
         avilable_next_actions = self.get_available_next_actions(state['state'])
         for action in avilable_next_actions:
-            action_id, action_position = action
+            action_position, action_id = action
             action_state = deepcopy(state)
             action_state['parent'] = selected_action_id
             if current_player == 'x':
@@ -65,12 +66,12 @@ class VanilaMCTS:
                 action_state['state'][action_position] = 1
 
             self.state_tree[selected_action_id]['child'].append(action_id)
-            child.append(selected_action_id + action_id)
-            self.state_tree[selected_action_id + action_id] = action_state
+            child.append(selected_action_id + (action_id,))
+            self.state_tree[selected_action_id + (action_id,)] = action_state
 
         random_action_choice = np.random.randint(low=0, high=len(child), size=1)[0]
         child_action_id = child[random_action_choice]
-        return child_action_id, len(child_action_id)
+        return child_action_id
     
 
     def is_terminal_state(self, leaf_state):
@@ -124,16 +125,18 @@ class VanilaMCTS:
         return actions
 
     def simulate_play(self, child_action_id):
+        self.total_play += 1
         state = deepcopy(self.state_tree[child_action_id])
         winner = self.is_terminal_state(self.state_tree[child_action_id]['state'])
         if winner:
             return winner
+    
         
         is_game_end = False
         while not is_game_end:
             available_actions = self.get_available_next_actions(state['state'])
             random_action_choice = np.random.randint(low=0, high=len(available_actions), size=1)[0]
-            action_id, action_position = available_actions[random_action_choice]
+            action_position, action_id = available_actions[random_action_choice]
             if state['player'] == 'x':
                 state['player'] = 'o'
                 state['state'][action_position] = -1
@@ -162,10 +165,12 @@ class VanilaMCTS:
                 break
               
 
-    def choose_best_action(self):
+    def solve(self):
+        play_depth = 0
         for _ in range(self.num_itearions):
             selected_action_id = self.select_potential_leaf_action()
-            child_action_id, play_depth = self.expand(selected_action_id)
+            child_action_id = self.expand(selected_action_id)
+            play_depth = len(selected_action_id)
             if play_depth >= self.max_depth:
                 break
             winner = self.simulate_play(child_action_id)
@@ -173,12 +178,12 @@ class VanilaMCTS:
         
         best_action_value, best_action = float('-inf'), None
         for action_id in self.state_tree[self.root_id]['child']:
-            action_state = self.state_tree[self.root_id + action_id]
+            action_state = self.state_tree[self.root_id + (action_id, )]
             if action_state['q'] > best_action_value:
                 best_action_value = action_state['q']
                 best_action = action_id
 
-        return best_action, best_action_value
+        return best_action, best_action_value, play_depth
                 
         
     
--- play.py
+++ play.py
@@ -1,6 +1,7 @@
 from env import env as game
 import numpy as np
-from my_mcts import VanilaMCTS
+# from my_mcts import VanilaMCTS
+from mct_try import VanilaMCTS
 import time
 
 env = game.GameState()
@@ -17,8 +18,10 @@ while not game_end:
     if current_player == mcts_player:
         print('game board')
         print(game_board)
-        mcts = VanilaMCTS(n_iterations=1500, depth=15, exploration_const=100, 
-                          game_board=game_board, player=current_player)
+        # mcts = VanilaMCTS(n_iterations=1500, depth=15, exploration_const=100, 
+        #                   game_board=game_board, player=current_player)
+        mcts = VanilaMCTS(num_iterations=1500, max_depth=15, explore_const=100, 
+                          initial_state=game_board, player=current_player)
         best_action, best_q, depth = mcts.solve()
         actione_onehot = np.zeros([state_size**2])
         actione_onehot[best_action] = 1
