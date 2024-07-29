e21622e3d29fbc3c27ed9db0234514429d96db08
carlfeynman <coolarun477@gmail.com>
2024-07-29 09:45:34 +0530

    mcts refactor started

--- pyspiel_mct.py
+++ pyspiel_mct.py
@@ -1,7 +1,23 @@
 import random
 
+def get_state_tree(state, root_id):
+    return {root_id: { 'state': state, 'parent': None, 'w':0, 'n': 1e-4, 'q': 0, 'child': []}}
 
+def select_leaf_node(state_tree, node_id):
+    child = state_tree[node_id]['child']
+    if len(child) == 0:
+        return node_id
 
-
+    best_leaf_node_id, best_score = None, float('-inf')
+    for child_id in child:
+        pass
+        
+     
 def select_action(state):
+    root_id = (0,)
+    state_tree = get_state_tree(state, root_id)
+    selected_leaf_node_id = select_leaf_node(state_tree, root_id)
+    
+
     return random.choice(state.legal_actions())
+
